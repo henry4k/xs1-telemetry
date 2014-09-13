c8a26fbb38f1ca3de3734543aaff22cf0acba856
Henry Kielmann <henrykielmann@gmail.com>
2014-09-13 20:39:12 +0200

    Implemented service tools in shell script.

--- service.sh
+++ service.sh
@@ -19,67 +19,60 @@ if [ -z "$LOG_FILE" ]; then
     LOG_FILE="${NAME}.log"
 fi
 
-
-function is_running
-{
-    if [ ! -e "$PID_FILE" ]; then
-        return 1
-    else
-        return 0
-    fi
-}
-
-function start
-{
-    if is_running; then
-        return 1
-    else
-        nohup $COMMAND >"$LOG_FILE" &
-        echo $! > "$PID_FILE"
-        return 0
-    fi
-}
-
-function stop
-{
-    if is_running; then
-        local pid=$(cat "$PID_FILE")
-        kill $pid
-        rm "$PID_FILE"
-        return 0
-    else
-        return 1
-    fi
-}
-
 case $1 in
     status)
-        if is_running; then
+        if $0 is_running; then
             echo "$NAME is running."
         else
             echo "$NAME is not running."
         fi
     ;;
     start)
-        if start; then
+        if $0 start_; then
             echo "Started $NAME."
         else
             echo "$NAME seems to be running already."
         fi
     ;;
     stop)
-        if stop; then
+        if $0 stop_; then
             echo "Stopped $NAME."
         else
             echo "$NAME is not running."
         fi
     ;;
     restart)
-        stop
-        if start; then
+        $0 stop_
+        if $0 start_; then
             echo "Restarted $NAME"
         else
             echo "Restarting of $NAME failed."
+        fi
+    ;;
+    is_running)
+        if [ ! -e "$PID_FILE" ]; then
+            return 1
+        else
+            return 0
+        fi
+    ;;
+    start_)
+        if $0 is_running; then
+            return 1
+        else
+            nohup $COMMAND >"$LOG_FILE" &
+            echo $! > "$PID_FILE"
+            return 0
+        fi
+    ;;
+    stop_)
+        if $0 is_running; then
+            local pid=$(cat "$PID_FILE")
+            kill $pid
+            rm "$PID_FILE"
+            return 0
+        else
+            return 1
         fi
     ;;
 esac
